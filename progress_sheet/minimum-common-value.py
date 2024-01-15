25e53127df2e1ad3e8fa6511834599a74cdd2b12
Joshua <87242555+josh-x01@users.noreply.github.com>
2024-01-15 19:26:20 +0300

    Added solution for Minimum Common Value

---
+++ progress_sheet/minimum-common-value.py
@@ -0,0 +1,10 @@
+class Solution:
+    def getCommon(self, nums1: List[int], nums2: List[int]) -> int:
+        for i in range(len(nums1)):
+            for j in range(len(nums2)):
+                if nums1[i] < nums2[j]:
+                    break
+                if nums1[i] == nums2[j]:
+                    return nums1[i]
+        return -1
+        
