b09a087ea26d127a59fcc3f18e05642ecdfaa040
Joshua <87242555+josh-x01@users.noreply.github.com>
2024-01-18 17:26:29 +0300

    Added solution for Maximum Sum of an Hourglass

---
+++ progress_sheet/maximum-sum-of-an-hourglass.py
@@ -0,0 +1,10 @@
+class Solution:
+    def maxSum(self, grid: List[List[int]]) -> int:
+        result=[]
+        sum_=0
+        for i in range(len(grid)-2):
+            for j in range(len(grid[0])-2):
+                sum_=grid[i][j]+grid[i][j+1]+grid[i][j+2]+grid[i+1][j+1]+grid[i+2][j]+grid[i+2][j+1]+grid[i+2][j+2]
+                result.append(sum_)
+    
+        return max(result)
