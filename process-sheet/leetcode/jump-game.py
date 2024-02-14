39d4ab23607100e7252f6694192d1b683b71ad5d
Joshua <87242555+josh-x01@users.noreply.github.com>
2024-02-14 15:38:24 +0300

    Add solution for Jump Game

---
+++ process-sheet/leetcode/jump-game.py
@@ -0,0 +1,7 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        last_good_index = len(nums) - 1
+        for i in range(len(nums) -1, -1, -1):
+            if i + nums[i] >= last_good_index:
+                last_good_index = i
+        return last_good_index == 0
