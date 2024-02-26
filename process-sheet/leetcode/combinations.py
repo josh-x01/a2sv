9d9b59ab1fdea9ef2a560913e0d676c64d0aa925
Joshua <87242555+josh-x01@users.noreply.github.com>
2024-02-26 14:40:11 +0300

    Add solution for Combinations

---
+++ process-sheet/leetcode/combinations.py
@@ -0,0 +1,17 @@
+class Solution:
+    @cache
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        def backtrack(first_num, path):
+            # print(first_num, path)
+            if len(path) == k:
+                ans.append(path[:])
+                return
+
+            for num in range(first_num, n+1):
+                path.append(num)
+                backtrack(num+1, path)
+                path.pop()
+        
+        ans = []
+        backtrack(1, [])
+        return ans
